73c6e678f3bf31293ea18aa6b92af26aac7847af
rheeze-dev <ggybzz2297@gmail.com>
2022-09-02 19:41:03 -0400

    Created the DbContext

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Call the base version of this method as well, else we get an error later on.
+            base.OnModelCreating(modelBuilder);
+
+            Category[] categoriesToSeed = new Category[3];
+
+            for (int i = 1; i < 4; i++)
+            {
+                categoriesToSeed[i - 1] = new Category
+                {
+                    CategoryId = i,
+                    ThumbnailImagePath = "uploads/placeholder.jpg",
+                    Name = $"Category {i}",
+                    Description = $"A description of category {i}"
+                };
+            }
+
+            modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+        }
+    }
+}
