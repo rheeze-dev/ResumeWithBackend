d3c88ebf9724033d4ea6dd843d092a3a6e0c05f9
rheeze-dev <ggybzz2297@gmail.com>
2022-09-02 20:57:10 -0400

    Created the API

---
+++ Server/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoriesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = await _appDbContext.Categories.ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
