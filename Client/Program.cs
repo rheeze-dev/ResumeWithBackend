8dc80206f7b28229323e1b57314bc7674aefe8fc
rheeze-dev <ggybzz2297@gmail.com>
2022-09-02 21:10:21 -0400

    Consuming API from Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddSingleton<InMemoryDatabaseCache>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services
+{
+    internal sealed class InMemoryDatabaseCache
+    {
+        private readonly HttpClient _httpClient;
+
+        public InMemoryDatabaseCache(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private List<Category> _categories = null;
+        internal List<Category> Categories
+        {
+            get { return _categories; }
+            set { _categories = value; NotifyCategoriesDataChanged(); }
+        }
+
+        internal async Task GetCategoriesFromDatabaseAndCache()
+        {
+            _categories = await _httpClient.GetFromJsonAsync<List<Category>>("endpoint");
+        }
+
+        internal event Action OnCategoriesDataChanged;
+        private void NotifyCategoriesDataChanged() => OnCategoriesDataChanged?.Invoke();
+    }
+}
