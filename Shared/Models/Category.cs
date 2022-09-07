85fb8753c7ce5ce5382cf57209e29d2d41178bb6
rheeze-dev <ggybzz2297@gmail.com>
2022-09-07 00:26:11 -0400

    Added custom model validations

--- Shared/Models/Category.cs
+++ Shared/Models/Category.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Shared.Models.CustomValidations;
 
 namespace Shared.Models
 {
@@ -6,15 +8,21 @@ namespace Shared.Models
     {
         [Key]
         public int CategoryId { get; set; }
+
         [Required]
         [MaxLength(256)]
         public string ThumbnailImagePath { get; set; }
+
         [Required]
         [MaxLength(128)]
+        [NoPeriods(ErrorMessage = "The category Name field contains one or more period characters (.). Please remove all periods.")]
+        [NoThreeOrMoreSpacesInARow(ErrorMessage = "The category Name field contains three or more spaces in a row. Please remove them.")]
         public string Name { get; set; }
+
         [Required]
         [MaxLength(1024)]
         public string Description { get; set; }
+
         public List<Post> Posts { get; set; }
     }
 }
